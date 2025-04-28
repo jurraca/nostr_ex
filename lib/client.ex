cdab6f6b66d24942acab8193f0718b2a816689ca
jurraca <julienu@pm.me>
2025-04-28 15:44:37 +0100

    move creation and send operations to Client module, simplify functions

--- lib/client.ex
+++ lib/client.ex
@@ -3,25 +3,105 @@ defmodule Nostrbase.Client do
     Wrap the Websocket Client in `WsClient`.
   """
 
-  def send_event(relay_pid, text) when is_binary(text) do
-    WebSockex.cast(relay_pid, {:send, {:text, text}})
+  alias Nostr.{Event, Filter, Message}
+  alias Nostrbase.{RelayAgent, RelayManager, Socket}
+
+  def send_note(note, privkey, opts \\ []) do
+    do_event_send(privkey, note, &create_note/2, opts)
   end
 
-  def subscribe(relay_pid, sub_id, encoded_filter) do
+  def send_long_form(text, privkey, opts \\ []) do
+    do_event_send(privkey, text, &create_long_form/2, opts)
+  end
+
+  def send_event(relay_pid, payload) when is_binary(payload) do
+    Socket.send_message(relay_pid, payload)
+  end
+
+  def send_sub(filter, opts \\ []), do: do_subscribe_send(filter, opts)
+
+  def close_sub(relay_pid, sub_id) when is_binary(sub_id) do
+    close_sub(relay_pid, String.to_existing_atom(sub_id))
+  end
+
+  def close_sub(relay_pid, sub_id) do
+    request = Nostr.Message.close(sub_id)
+    send_event(relay_pid, request)
+    RelayAgent.delete(relay_pid, sub_id)
+  end
+
+  def close_conn(relay_pid) do
+    # close all subs for that relay, then terminate
+    DynamicSupervisor.terminate_child(RelayManager, relay_pid)
+  end
+
+  def create_note(note, privkey) do
+      note
+      |> Event.Note.create()
+      |> Event.sign(privkey)
+      |> Message.create_event()
+      |> Message.serialize()
+  end
+
+  def create_long_form(text, privkey) do
+    Event.create(23, [content: text])
+    |> Event.sign(privkey)
+    |> Message.create_event()
+    |> Message.serialize()
+  end
+
+  def create_sub(opts) when is_list(opts) do
+     with filter <- Map.merge(%Filter{}, Enum.into(opts, %{})),
+       sub_id <- :crypto.strong_rand_bytes(32) |> Base.encode16(case: :lower) do
+       msg = [filter]
+        |> Message.request(sub_id)
+        |> Message.serialize()
+
+       {:ok, sub_id, msg}
+     end
+  end
+
+  def subscribe(relay_pid, sub_id, payload) when is_binary(sub_id) do
+    subscribe(relay_pid, String.to_atom(sub_id), payload)
+  end
+
+  def subscribe(relay_pid, sub_id, payload) when is_atom(sub_id) do
     with {:ok, _pid} <- Registry.register(Nostrbase.PubSub, sub_id, []),
-         :ok <- WebSockex.cast(relay_pid, {:send, {:text, encoded_filter, sub_id}}),
+         :ok <- send_event(relay_pid, payload),
          :ok <- Nostrbase.RelayAgent.update(relay_pid, sub_id) do
       {:ok, sub_id}
     end
   end
 
-  def close_sub(relay_pid, sub_id) do
-    request = Nostrlib.CloseRequest.new(sub_id)
-    send_event(relay_pid, request)
-    Nostrbase.RelayAgent.delete(relay_pid, sub_id)
+  defp do_event_send(privkey, arg, create_fun, opts) do
+    with {:ok, relay_pids} <- get_relays(opts[:send_via]),
+         json_event <- create_fun.(arg, privkey) do
+      Enum.each(relay_pids, &send_event(&1, json_event))
+    else
+      {:error, reason} -> {:error, reason}
+      _ -> {:error, "Invalid event submitted with argument \"#{arg}\" "}
+    end
   end
 
-  def close_conn(relay_pid) do
-    DynamicSupervisor.terminate_child(RelayManager, relay_pid)
+  defp do_subscribe_send(filter, opts) do
+    with {:ok, relay_pids} <- get_relays(opts[:send_via]),
+         {:ok, sub_id, message} <- create_sub(filter) do
+      # validate all responses and collect errors
+      Enum.each(relay_pids, &subscribe(&1, sub_id, message))
+    else
+      {:error, reason} -> {:error, reason}
+    end
   end
+
+  defp get_relays(nil), do: get_relays(:all)
+  defp get_relays(:all), do: {:ok, RelayManager.active_pids()}
+
+  defp get_relays([_h | _t] = relay_list) do
+    case Enum.all?(relay_list, &is_pid(&1)) do
+      false -> {:error, "One or more relay PIDs provided were invalid."}
+      true -> {:ok, relay_list}
+    end
+  end
+
+  defp get_relays(relay_list), do: {:error, "invalid relay list provided, got: #{relay_list}"}
 end
--- lib/nostrbase.ex
+++ lib/nostrbase.ex
@@ -4,12 +4,12 @@ defmodule Nostrbase do
   """
 
   alias Nostrbase.{Client, RelayAgent, RelayManager}
-  alias Nostrlib.{Filter, Note}
 
   @doc """
   Connect to a relay via its URL.
   """
   def add_relay(relay_url) do
+    # relay_url in RelayManager.relays() ?
     case RelayManager.connect(relay_url) do
       {:ok, _pid} = res -> res
       {:error, _} = err -> err
@@ -18,52 +18,30 @@ defmodule Nostrbase do
   end
 
   @doc """
-  Sends a text note via relays
-  opts should include a :send_via key indicating relay_pids to send this note to. See get_relays/1.
+    Sends a text note via relays
+    opts should include a :send_via key indicating relay_pids to send this note to. See get_relays/1.
   """
-  def send_note(note, privkey, opts \\ []) do
-    with {:ok, relay_pids} <- get_relays(opts[:send_via]),
-         {:ok, json_event} <- Note.new(%{content: note}) |> Note.to_event_serialized(privkey) do
-      Enum.each(relay_pids, &Client.send_event(&1, json_event))
-    else
-      {:error, reason} -> {:error, reason}
-      _ -> {:error, "Invalid event submitted for note \"#{note}\" "}
-    end
-  end
+  def send_note(note, privkey, opts \\ []), do: Client.send_note(note, privkey, opts)
+
+  def send_long_form(text, privkey, opts \\ []), do: Client.send_long_form(text, privkey, opts)
 
   @doc """
-    Send a REQ message to a relay, by providing a json-encoded filter, a subscription ID, and optionally, relay conn PIDs with a `opts[:send_via]` list.
+    Send a REQ message to a relay, by providing a keyword list of filter values, and optionally, relay conn PIDs with a `opts[:send_via]` list.
   """
-  def send_subscription(sub_id, filter, opts \\ []) when is_binary(filter) do
-    with {:ok, relay_pids} <- get_relays(opts[:send_via]) do
-      Enum.each(relay_pids, &Client.subscribe(&1, sub_id, filter))
-    else
-      {:error, reason} -> {:error, reason}
-    end
-  end
+  def send_subscription(filter, opts \\ []), do: Client.send_sub(filter, opts)
 
   @doc """
     Subscribe to a pubkey's notes, i.e. "follow" a pubkey.
   """
-  def subscribe_to_pubkey(pubkey, opts \\ []) do
-    with {:ok, filter} <- Filter.notes([pubkey]),
-         sub_id <- Filter.create_sub_id(),
-         {:ok, req_string} <- Filter.encode(sub_id, filter) do
-      case send_subscription(sub_id, req_string, opts) do
-        {:error, reason} -> {:error, reason}
-        _ -> {:ok, sub_id}
-      end
-    end
+  def subscribe_pubkey(pubkey, opts \\ []) do
+    send_subscription([authors: [pubkey], kinds: [1]], opts)
   end
 
   @doc """
     Get a pubkey's contact list.
   """
-  def get_contacts(pubkey, opts \\ []) do
-    with {:ok, filter} <- Filter.contacts(pubkey),
-         {:ok, req} <- Filter.create_sub_id() |> Filter.encode(filter) do
-      send_subscription("", req, opts)
-    end
+  def subscribe_contacts(pubkey, opts \\ []) do
+    send_subscription([authors: [pubkey], kinds: [3]], opts)
   end
 
   def close_all_subs do
@@ -73,16 +51,4 @@ defmodule Nostrbase do
   end
 
   def listen_for_sub(sub_id), do: Registry.register(Nostrbase.PubSub, sub_id, [])
-
-  defp get_relays(nil), do: get_relays(:all)
-  defp get_relays(:all), do: {:ok, RelayManager.active_pids()}
-
-  defp get_relays([_h | _t] = relay_list) do
-    case Enum.all?(relay_list, &is_pid(&1)) do
-      false -> {:error, "One or more relay PIDs provided were invalid."}
-      true -> {:ok, relay_list}
-    end
-  end
-
-  defp get_relays(relay_list), do: {:error, "invalid relay list provided, got: #{relay_list}"}
 end
