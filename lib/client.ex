9ec2c6269201210b9b17ed2f745fec83c2070369
jurraca <julienu@pm.me>
2025-06-11 16:53:49 +0200

    client: add documentation and fix some guards; wip

--- lib/client.ex
+++ lib/client.ex
@@ -1,23 +1,46 @@
 defmodule Nostrbase.Client do
   @moduledoc """
-    Wrap the Websocket Client in `Socket`.
+    Transmit notes and other stuff via relays.
+
+    Mainly wraps the websocket wlient in `Socket`. When using functions like `send_note` or similar,
+    the `opts` arguments will take a `send_via` option which takes relays to send the event to.
+    By default it will send to all relays.
   """
 
   alias Nostr.{Event, Filter, Message}
   alias Nostrbase.{RelayAgent, RelayManager, Socket, Utils}
 
-  def send_event(relay_name, payload) when is_binary(payload) do
-    Socket.send_message(relay_name, payload)
+  @doc """
+    Send a serialized payload to the `relay`.
+    `relay` can either be a PID or a relay name as registered in the `RelayRegistry`.
+  """
+  def send_event(relay, payload) when is_binary(payload) do
+    Socket.send_message(relay, payload)
   end
 
+  @doc """
+    Send a kind 1 note.
+    Valid `opts`:
+      - `send_via`: a list of relays to send the event to.
+  """
   def send_note(note, privkey, opts \\ []) do
     do_event_send(privkey, note, &create_note/2, opts)
   end
 
+  @doc """
+    Send a kind 23 long-form note.
+    Valid `opts`:
+      - `send_via`: a list of relays to send the event to.
+  """
   def send_long_form(text, privkey, opts \\ []) do
     do_event_send(privkey, text, &create_long_form/2, opts)
   end
 
+  @doc """
+    Send a subscription request.
+    `filter` is a keyword list of arguments for a `Nostr.Filter` struct.
+    Since subscriptions are asynchronous.
+  """
   def send_sub(filter, opts \\ []) do
     with {:ok, sub_id, message} <- create_sub(filter),
          {:ok, _pid} <- Registry.register(Nostrbase.PubSub, sub_id, nil) do
@@ -33,12 +56,16 @@ defmodule Nostrbase.Client do
     end
   end
 
+  @doc """
+    Close a subscription with ID `sub_id`.
+    The subscription `CLOSE` message will be sent to all relays which know about this subscription.
+  """
   def close_sub(sub_id) when is_binary(sub_id) do
     with true <- sub_id in RelayAgent.get_unique_subscriptions(),
          relays <- RelayAgent.get_relays_for_sub(sub_id),
          request = Message.close(sub_id) |> Message.serialize() do
       relays
-        |> Enum.map(fn relay_name ->
+      |> Enum.map(fn relay_name ->
         case send_event(relay_name, request) do
           {:ok, _} -> RelayAgent.delete_subscription(relay_name, sub_id)
           err -> err
@@ -48,11 +75,15 @@ defmodule Nostrbase.Client do
     else
       false ->
         {:error, "subscription ID not found: #{sub_id}"}
+
       _ ->
         {:error, "could not get relays for sub_id: #{sub_id}"}
     end
   end
 
+  @doc """
+    Close a connection to a relay.
+  """
   def close_conn(relay_name) do
     case Registry.lookup(Nostrbase.RelayRegistry, relay_name) do
       [{pid, _}] -> DynamicSupervisor.terminate_child(RelayManager, pid)
@@ -60,16 +91,25 @@ defmodule Nostrbase.Client do
     end
   end
 
-  def create_note(note, privkey) do
+  @doc """
+    Create a kind 1 event with content `note`, signed and serialized.
+  """
+  def create_note(note, privkey) when is_binary(note) do
     note
     |> Event.Note.create()
     |> sign_and_serialize(privkey)
   end
 
+  @doc """
+    Create a kind 23 event with content `text`, signed and serialized.
+  """
   def create_long_form(text, privkey) do
     Event.create(23, content: text) |> sign_and_serialize(privkey)
   end
 
+  @doc """
+    Create a subscription with arguments `opts`, which must be valid fields for a `Filter` struct.
+  """
   def create_sub(opts) when is_list(opts) do
     cond do
       # Single filter as keyword list
@@ -88,6 +128,9 @@ defmodule Nostrbase.Client do
     end
   end
 
+  @doc """
+    Subscribe with a serialized "REQ" `payload` with ID `sub_id` via relay with registered name `relay_name`.
+  """
   def subscribe(relay_name, sub_id, payload) when is_binary(sub_id) do
     with :ok <- send_event(relay_name, payload),
          :ok <- RelayAgent.update(relay_name, sub_id) do
@@ -97,12 +140,18 @@ defmodule Nostrbase.Client do
 
   def subscribe(_, sub_id, _), do: {:error, "invalid sub_id format, got #{sub_id}"}
 
-  def sign_and_serialize(event, privkey) do
+  @doc """
+    Sign and serialize an `event` with private key `privkey`.
+  """
+  def sign_and_serialize(%Event{} = event, privkey) do
     event
     |> Event.sign(privkey)
     |> Message.create_event()
     |> Message.serialize()
   end
+
+  def sign_and_serialize(_, _),
+    do: {:error, "invalid event provided, must be an %Event{} struct."}
 
   defp do_event_send(privkey, arg, create_fun, opts) do
     with relay_names = get_relays(opts[:send_via]),
@@ -135,6 +184,7 @@ defmodule Nostrbase.Client do
       cond do
         relay in RelayManager.registered_names() -> relay
         is_binary(relay) -> relay |> URI.parse() |> Map.get(:host) |> Utils.name_from_host()
+        is_pid(relay) and relay in RelayManager.active_pids() -> relay
         true -> {:error, "invalid relay name, got #{relay}"}
       end
     end)
