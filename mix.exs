fe1947b8dd8434817a205fe1d8f08f8a8af90ff6
jurraca <julienu@pm.me>
2025-06-27 17:14:23 +0200

    add package metadata and docs info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,32 @@ defmodule NostrEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
+    ]
+  end
+
+  defp package do
+    [
+      name: "nostr_ex",
+      maintainers: ["jurraca <julienu@pm.me"],
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/jurraca/nostr_ex",
+        "Nostr specs" => "https://github.com/nostr-protocol/nips"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      authors: ["jurraca <julienu@pm.me>"],
+      main: "overview",
+      api_reference: false,
+      formatters: ["html"],
+      source_url: "https://github.com/jurraca/nostr_ex"
     ]
   end
 
