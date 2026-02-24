a1caac7ed5517529f1d8d98c87e757cc040ad8a7
jurraca <julienu@pm.me>
2026-02-24 17:32:15 +0000

    register the PID with the Registry before sending the subscription to the relay

--- lib/client.ex
+++ lib/client.ex
@@ -102,11 +102,11 @@ defmodule NostrEx.Client do
         {:error, "no relays connected"}
 
       _ ->
+        {:ok, _pid} = Registry.register(NostrEx.PubSub, sub_id, nil)
         Enum.each(relay_names, fn relay_name ->
           subscribe_to_relay(relay_name, sub_id, message)
         end)
 
-        {:ok, _pid} = Registry.register(NostrEx.PubSub, sub_id, nil)
         {:ok, sub_id}
     end
   end
